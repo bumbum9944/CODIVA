af3139fe03e6a80a148424acfc91ee10e38b8d14
bumbum9944 <tong940526@gmail.com>
2021-05-21 19:58:28 +0900

    feat: carousel 크기 수정 - 메인 페이지 carousel 크기 수정

--- Frontend/src/components/Home/HomeSearchButton.js
+++ Frontend/src/components/Home/HomeSearchButton.js
@@ -5,7 +5,12 @@ import Button from "@material-ui/core/Button";
 function HomeSearchButton() {
   return (
     <div className="searchButton-container">
-      <Button className="searchButton" style={{fontSize: "2vh"}} variant="outlined" color="secondary">
+      <Button
+        className="searchButton"
+        style={{ fontSize: "2vh" }}
+        variant="outlined"
+        color="secondary"
+      >
         CODY MATCH
       </Button>
     </div>
