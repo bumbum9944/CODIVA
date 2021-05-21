62f710d773a8a5a0631a28af0ae32144bb39e38e
bumbum9944 <tong940526@gmail.com>
2021-05-21 15:42:24 +0900

    feat: 메인 페이지 디자인 수정 - 캐로셀 사진 크기 조절

--- Frontend/src/components/Home/HomeCarousel.js
+++ Frontend/src/components/Home/HomeCarousel.js
@@ -1,42 +1,16 @@
 import { React } from "react";
 import Slider from "react-slick";
-import styled from "styled-components";
+import "./HomeCarousel.css"
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 function HomeCarousel() {
-  const Container = styled.div`
-    overflow: hidden;
-    height: 70vh; 
-  `;
-
-  const WhiteSpace = styled.div`
-    height: 5vh;
-  `;
-  const StyledSlider = styled(Slider)`
-    .slick-slide div {
-      outline: none;
-    }
-  `;
-
-  const ImageContainer = styled.div`
-    margin: 0 16px;
-  `;
-
-  const Image = styled.img`
-    width: 60vw;
-    height: 60vh;
-  `;
 
   const sliderInner = [];
 
   for (let i = 0; i < 5; i++) {
     const item = (
-      <div key={i}>
-        <ImageContainer style={{margin: "0",}}>
-          <Image style={{margin: "0",}} src={`/carouselImage/item${i+1}.jpg`} alt="item-img" />
-        </ImageContainer>
-      </div>
+      <img className="home-carousel-image" key={i} src={`/carouselImage/item${i + 1}.jpg`} alt="item-img" />
     );
     sliderInner.push(item);
   }
@@ -49,14 +23,12 @@ function HomeCarousel() {
     slidesToShow: 1,
     slidesToScroll: 1,
     arrows: false,
-    centerMode: true
   };
 
   return (
-    <Container>
-      <WhiteSpace />
-      <StyledSlider {...settings}>{sliderInner}</StyledSlider>
-    </Container>
+    <div className="home-carousel-container">
+      <Slider className="home-carousel" {...settings}>{sliderInner}</Slider>
+    </div>
   );
 }
 
