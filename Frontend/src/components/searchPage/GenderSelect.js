01d4751f0b80a3b47fc55ab0d6de64b3dfc9d05a
yunk621 <dbsk621@naver.com>
2021-05-23 14:58:52 +0900

    feat: 검색 페이지 구현(1)

---
+++ Frontend/src/components/searchPage/GenderSelect.js
@@ -0,0 +1,63 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import ButtonBase from "@material-ui/core/ButtonBase";
+import Typography from "@material-ui/core/Typography";
+
+const images = [
+  {
+    url: "/images/image_man.png",
+    title: "Man",
+    width: "33%"
+  },
+  {
+    url: "/images/image_woman.png",
+    title: "Woman",
+    width: "33%"
+  },
+  {
+    url: "/images/image_all.png",
+    title: "All",
+    width: "34%"
+  }
+];
+
+function GenderSelect() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      {images.map(image => (
+        <ButtonBase
+          focusRipple
+          key={image.title}
+          className={classes.image}
+          focusVisibleClassName={classes.focusVisible}
+          style={{
+            width: image.width
+          }}
+        >
+          <span
+            className={classes.imageSrc}
+            style={{
+              backgroundImage: `url(${image.url})`
+            }}
+          />
+          <span className={classes.imageBackdrop} />
+          <span className={classes.imageButton}>
+            <Typography
+              component="span"
+              variant="subtitle1"
+              color="inherit"
+              className={classes.imageTitle}
+            >
+              {image.title}
+              <span className={classes.imageMarked} />
+            </Typography>
+          </span>
+        </ButtonBase>
+      ))}
+    </div>
+  );
+}
+
+export default GenderSelect;
