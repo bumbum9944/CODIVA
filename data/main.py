ad3cceedd9bad7a89c1b1dc7842a5b638d9e6c17
Sowon Jung <swj960515@gmail.com>
2021-05-25 00:53:53 +0900

    refactor: requests 모듈적용
    - 기존 selenium 모듈에 의한 캐시 과도 사용으로 크롤링 속도가 늦어지는
    현상을 방지하기 위해 requests 모듈을 사용한 크롤링 방식으로 전환

--- data/main.py
+++ data/main.py
@@ -1,39 +1,54 @@
 from crawling import ImgCrawler
 from resize import img_resizer
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
+
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
-import time, dload, asyncio, json
+import time, dload, asyncio, json, os, requests
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+headers = {
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36"
+}
 
 
-async def crawling(url, selector):
+async def crawling(url, selector, id):
     links = []
-    dname = "top/tee"  # 세부 디렉토리 명칭
-    fname = "short_sleeved_tee"  # 파일명칭
-    i = 1  # 파일 넘버링
-
-    driver.get(url)
-    await asyncio.sleep(1.0)
-    soup = BeautifulSoup(driver.page_source, "html.parser")
+    dname = "bottom/slacks"  # 세부 디렉토리 명칭
+    fname = "slacks"  # 파일명칭
+    # driver.get(url)
+    try:
+        res = requests.get(url, headers=headers)
+        # print(type(res.headers.get("X-Amz-Cf-Id")))
+    except:
+        print("requests error")
+    soup = BeautifulSoup(res.text, "html.parser")
     links += soup.select("a.img-block")
-
     images = await get_images(links)
     for image in images:
-        dload.save("https:" + image["src"], f"./train_images/{dname}/{fname}{i}.jpg")
-        i += 1
+        dload.save(image, f"./train_images/{dname}/{fname}{id}.jpg")
+        id += 1
+    return id
 
 
 async def get_images(links):
     images, cache = [], {}
     for link in links:
-        driver2.get(link["href"])
-        await asyncio.sleep(1.0)
-        soup = BeautifulSoup(driver2.page_source, "html.parser")
-        images += soup.select("#bigimg")
-    cache[str(time.ctime)] = images  # 현재 시간을 key로 cache 저장(크롤링 중단 대비)
+        try:
+            res = requests.get(link["href"], headers=headers)
+            print(res.headers.get("X-Amz-Cf-Id"))
+        except:
+            print("request error")
+            continue
+        soup = BeautifulSoup(res.text, "html.parser")
+        url = soup.select("#bigimg")[0]["src"]
+        if soup.select("#bigimg")[0]["src"].startswith("//"):
+            url = "https:" + url
+        images += [url]
+    cache[res.headers.get("X-Amz-Cf-Id")] = images  # 현재 시간을 key로 cache 저장(크롤링 중단 대비)
     print(cache)
-    with open("./cache.json", "w") as f:
+    with open(os.path.join(BASE_DIR, "cache.json"), "w", encoding="utf-8") as f:
         json.dump(cache, f, indent="\t")
     return images
 
@@ -48,13 +63,14 @@ if __name__ == "__main__":
     # crawler.quit()
 
     # 무신사 상품(학습) 데이터 크롤링
-    driver = webdriver.Chrome("./chromedriver")
-    driver2 = webdriver.Chrome("./chromedriver")
+    # driver = webdriver.Chrome("./chromedriver")
+    # driver2 = webdriver.Chrome("./chromedriver")
 
     # 각 상품 상세페이지로 들어갈 하이퍼링크 수집
-    for page_num in range(1, 11):
-        url = f"https://search.musinsa.com/category/001001?page={page_num}"
-        asyncio.run(crawling(url, "a.img-block"))
+    i = 1
+    for page_num in range(1, 15):
+        url = f"https://search.musinsa.com/category/003008?page={page_num}"
+        i = asyncio.run(crawling(url, "a.img-block", i))
 
     # 각 상품에 대한 대표 이미지 수집
     # images = []
