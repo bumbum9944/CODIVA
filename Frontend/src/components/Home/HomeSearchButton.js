4d5c2775db46f8f387e72586e8b15c83ad531d82
yunk621 <dbsk621@naver.com>
2021-05-24 17:51:31 +0900

    feat: 메인 버튼에 코디 검색을 위한 링크 연결

--- Frontend/src/components/Home/HomeSearchButton.js
+++ Frontend/src/components/Home/HomeSearchButton.js
@@ -1,18 +1,21 @@
 import { React } from "react";
 import "./HomeSearchButton.css";
 import Button from "@material-ui/core/Button";
+import { Link } from "react-router-dom";
 
 function HomeSearchButton() {
   return (
     <div className="searchButton-container">
-      <Button
-        className="searchButton"
-        style={{ fontSize: "2vh" }}
-        variant="outlined"
-        color="secondary"
-      >
-        CODY MATCH
-      </Button>
+      <Link to="/search/1">
+        <Button
+          className="searchButton"
+          style={{ fontSize: "2vh" }}
+          variant="outlined"
+          color="secondary"
+        >
+          CODY MATCH
+        </Button>
+      </Link>
     </div>
   );
 }
