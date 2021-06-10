426cb56f3f0f3ff3e97ad913dac7bcfaed8176c1
Sowon Jung <swj960515@gmail.com>
2021-06-11 01:10:35 +0900

    fix: top codies api에서 like count null로 보내지는 버그 수정

--- Backend/app/resources/codi/__init__.py
+++ Backend/app/resources/codi/__init__.py
@@ -15,7 +15,7 @@ def get_codies():
     count = query["count"] if "count" in query else 20
     with connect_db() as connection:
         with connection.cursor() as cursor:
-            sql = f"SELECT c.id, c.url, c.apparels, c.hits, l.cnt from `codies` as c LEFT JOIN (SELECT codi_id as id, count(*) as cnt FROM likes GROUP BY codi_id) as l ON c.id = l.id ORDER BY cnt DESC LIMIT {count}"
+            sql = f"SELECT id, url, apparels, hits, likes_cnt from `codies` ORDER BY likes_cnt DESC LIMIT {count}"
             cursor.execute(sql)
             res = cursor.fetchall()
             for r in res:
